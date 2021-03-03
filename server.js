fac6ed588cafb2e89ead01c544b6365815a6fe50
Emre Isik <gardiyas@Emres.noreply.github.com>
2021-03-02 22:44:12 -0500

    new

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const exphbs = require('express-handlebars');
 const url = require('url');
+const Sequelize = require('sequelize');
+const bcrypt = require('bcryptjs');
 
 app.engine('.hbs', exphbs({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
@@ -30,6 +32,38 @@ function onHttpStart() {
     console.log("Express http server listening on: " + HTTP_PORT);
 }
 
+const user = {
+    email: "",
+    password: "",
+    fname: "",
+    lname: ""
+};
+
+/* ********************* DATABASE INFORMATIONS *************************/
+var sequelize = new Sequelize('db9i6jmb7cnkdq', 'rgttdzkgrkibdo', '2684e4cdd8d0756de49f7714b043fa6feb35df8a01eb1cff1e8e3358e62862c9', {
+    host: 'ec2-52-202-66-191.compute-1.amazonaws.com',
+    dialect: 'postgres',
+    port: 5432,
+    dialectOptions: {
+        ssl: { rejectUnauthorized: false }
+    }
+});
+/* ********************* CREATING TABLE - WITH UNIQUE EMAIL *************************/
+
+var userTable = sequelize.define('Users', {
+    email: Sequelize.STRING,
+    fName: Sequelize.STRING,
+    lName: Sequelize.STRING,
+    psw: Sequelize.STRING,
+    bday: Sequelize.STRING
+});
+/* **************************** CREATING TABLE  ********************************/
+var itemTable = sequelize.define('Items', {
+    filename: Sequelize.STRING,
+    title: Sequelize.STRING,
+    price: Sequelize.STRING,
+    description: Sequelize.STRING    
+});
 
 /* ***************************** PAGES and DETAILS  **********************************/
 
@@ -39,7 +73,8 @@ app.get('/', (req, res) => {
     });
 });
 app.get('/register.html', (req, res) => {
-    res.render('register', {        
+    res.render('register', {  
+        user: req.user,      
         layout: false,              
     });
 });
@@ -53,82 +88,103 @@ app.get('/onthemenu.html', function(req,res){
 
 app.get('/login.html', function(req,res){
     res.render('login', {
+        user: req.user,
         layout: false
     });
     //res.sendFile(path.join(__dirname, 'listing.hbs'));
 });
 
+app.get("/logout", function (req, res) {
+    res.redirect("/login.html");
+});
+
 app.post("/register.html", (req, res) => {  
 
-    function check(){
-        if(req.body.email){
-            return true;
-        }else{
-            return false;
-        }
-    };  
-    res.render('dashboard', {
-        check: check(),
-        layout: false,
-        name: req.body.firstname + " " + req.body.lastname,
-        email: req.body.email,
-        psw: req.body.psw,
-        mon: req.body.mon,
-        day: req.body.day,
-        year: req.body.year          
-    }).catch(() => {
-        console.log("ERROR: Same email used!");
-        res.render("register", { errorMsg: "This email account is already registered!", layout: false });
-    });
-        
-});
+    bcrypt.hash(req.body.psw, 10).then(hash => {
 
+        console.log(hash);  // to print hashed password on console to test
 
-
-app.post("/login.html", (req, res) => {  
-        
-        function check(){
-            if(req.body.firstname){
-                return true;
-            }else{
-                return false;
-            }
-        };
-        res.render('dashboard', {
-            check: check(),
-            layout: false,
-            name: req.body.firstname + " " + req.body.lastname,
+        userTable.create({
             email: req.body.email,
-            psw: req.body.psw,
-            mon: req.body.mon,
-            day: req.body.day,
-            year: req.body.year
-        });       
+            fName: req.body.firstname,
+            lName: req.body.lastname,
+            psw: hash,
+            bday: req.body.mon + "-" + req.body.day + "-" + req.body.year
+        }).then(() => {
+            console.log("successfully created a new user");
+            res.render("thanks", { ifRegister: true, layout: false });
+        }).catch(() => {
+            console.log("ERROR: Same email used!");
+            res.render("register", { errorMsg: "This email account is already registered!", layout: false });
+        });
+    })
+        .catch(err => {
+            console.log(err); // Show any errors that occurred during the process
+        });  
+     
 });
 
-app.get("/logout", function (req, res) {
-        res.redirect("/login.html");
-});
-
-app.post("/dashboard", (req, res) => {
-    function check(){
-        if(req.body.firstname){
-            return true;
-        }else{
-            return false;
+app.post("/login.html", (req, res) => {       
+        
+    userTable.findAll({
+        attributes: ['email', 'psw', 'fName', 'lName'],
+        where: {
+            email: req.body.email
         }
-    }
-    res.render('dashboard', {
-        check: check(),
-        layout: false,
-        name: req.body.firstname + " " + req.body.lastname,
-        email: req.body.email,
-        psw: req.body.psw,
-        mon: req.body.mon,
-        day: req.body.day,
-        year: req.body.year
+    }).then(function (data) {
+        data = data.map(value => value.dataValues);
+
+        console.log("User info from data:");
+        console.log(data[0].email);
+        console.log(data[0].psw);
+
+        // Assign to use on different pages throughout session duration
+        user.email = data[0].email;
+        user.password = data[0].psw;
+        user.fname = data[0].fName;
+        user.lname = data[0].lName;
+
+        // console.log(req.body.email);
+        // console.log(req.body.psw);   
+
+        if (req.body.email === "" || req.body.psw === "") {
+            return res.render("login", { errorMsg: "Missing credentials.", layout: false });
+        }
+
+        bcrypt.compare(req.body.psw, user.password).then((result) => {
+            if (req.body.email === user.email && result === true) {
+
+                // Add the user on the session and redirect them to the dashboard page.
+                req.user = {
+                    email: user.email,
+                    password: user.password
+                };
+                res.redirect("/dashboard");
+            } else {
+                res.render("login", { errorMsg: "invalid username and/or password!", layout: false });
+            }
+        });
+
+    }).catch(function (e) {
+        console.log("ERROR: Undefined email value!");
+        res.render("login", { errorMsg: "invalid username and/or password!", layout: false });
     });       
-    //res.send("the first name is " + req.body.firstname + " and the last name is " + req.body.lastname);
+});
+
+app.get("/dashboard", (req, res) => {
+    itemTable.findAll({
+        order: ["id"]
+    }).then((data) => {
+        data = data.map(value => value.dataValues);
+        res.render("dashboard", {
+            data: data, 
+            layout: false, 
+            user: req.user,            
+            FIRSTNAME: user.fname + " ",
+            LASTNAME: user.lname,            
+        });
+    });      
+    
 });
 
 app.listen(HTTP_PORT, onHttpStart);
